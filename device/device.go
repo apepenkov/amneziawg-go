ced6dea6f2f09c257c7c424a4fa7d0d5dd9c0d22
apepenkov <apepenkov@users.noreply.github.com>
2024-11-01 14:07:09 +0300

    rename module

--- device/device.go
+++ device/device.go
@@ -11,11 +11,11 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/amnezia-vpn/amneziawg-go/conn"
-	"github.com/amnezia-vpn/amneziawg-go/ipc"
-	"github.com/amnezia-vpn/amneziawg-go/ratelimiter"
-	"github.com/amnezia-vpn/amneziawg-go/rwcancel"
-	"github.com/amnezia-vpn/amneziawg-go/tun"
+	"github.com/apepenkov/amneziawg-go/conn"
+	"github.com/apepenkov/amneziawg-go/ipc"
+	"github.com/apepenkov/amneziawg-go/ratelimiter"
+	"github.com/apepenkov/amneziawg-go/rwcancel"
+	"github.com/apepenkov/amneziawg-go/tun"
 	"github.com/tevino/abool/v2"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/amnezia-vpn/amneziawg-go
+module github.com/apepenkov/amneziawg-go
 
 go 1.22.3
 
